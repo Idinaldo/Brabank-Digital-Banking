507493e19c6ad1ea81ab2d71c25ff72011467c43
Idinaldo <idinaldooliveira220@gmail.com>
2026-02-18 20:36:01 -0300

    refactor: move entities/ to adapters/ (both directories) for a better alignment (visually) with Hexagonal Architecture

---
+++ auth/src/main/java/dev/idinaldo/brabank/auth/adapters/out/entities/JpaIdentity.java
@@ -0,0 +1,39 @@
+package dev.idinaldo.brabank.auth.adapters.out.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Identity")
+public class JpaIdentity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull(message = "National ID is mandatory")
+    @Column(nullable = false)
+    private String nationalIdMac;
+
+    @NotNull(message = "National ID is mandatory")
+    @Column(nullable = false)
+    private String encryptedNationalId;
+
+    @NotNull(message = "Password is mandatory")
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @CreationTimestamp
+    private Instant createdAt;
+    @UpdateTimestamp
+    private Instant updatedAt;
+}
