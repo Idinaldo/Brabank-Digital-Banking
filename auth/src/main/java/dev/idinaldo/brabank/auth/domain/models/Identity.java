2887f22ca91d3c39d00d0a0a443de6b0995f6972
Idinaldo <idinaldooliveira220@gmail.com>
2026-02-18 20:27:16 -0300

    feat: implement getters, setters and constructors

--- auth/src/main/java/dev/idinaldo/brabank/auth/domain/models/Identity.java
+++ auth/src/main/java/dev/idinaldo/brabank/auth/domain/models/Identity.java
@@ -1,6 +1,7 @@
 package dev.idinaldo.brabank.auth.domain.models;
 
 import dev.idinaldo.brabank.auth.domain.valueObjects.NationalId;
+import jakarta.persistence.Id;
 
 import java.util.UUID;
 
@@ -9,4 +10,41 @@ public class Identity {
     private UUID id;
     private NationalId nationalId;
     private String password;
+
+    public Identity(UUID id, NationalId nationalId, String password) {
+        this.id = id;
+        this.nationalId = nationalId;
+        this.password = password;
+    }
+
+    public Identity(NationalId nationalId, String password) {
+        this.nationalId = nationalId;
+        this.password = password;
+    }
+
+    public Identity() {}
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public NationalId getNationalId() {
+        return nationalId;
+    }
+
+    public void setNationalId(NationalId nationalId) {
+        this.nationalId = nationalId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
