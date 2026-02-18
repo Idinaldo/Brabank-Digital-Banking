fc5743dd12f7f5df6c9f3407ebd70091d4229997
Idinaldo <idinaldooliveira220@gmail.com>
2026-02-18 00:08:22 -0300

    feat: add exception default constructor and comments

--- auth/src/main/java/dev/idinaldo/brabank/auth/infrastructure/exceptions/InvalidNationalid.java
+++ auth/src/main/java/dev/idinaldo/brabank/auth/infrastructure/exceptions/InvalidNationalid.java
@@ -1,4 +1,10 @@
 package dev.idinaldo.brabank.auth.infrastructure.exceptions;
 
-public class InvalidNationalid extends Throwable {
+public class InvalidNationalid extends RuntimeException {
+
+    // default message
+    // this is the pattern Brabank follows to avoid account mapping
+    public InvalidNationalid() {
+        super("Please verify your data.");
+    }
 }
