188307823516e2af3302b5d57f4a1bf9ab7e1574
Idinaldo <idinaldooliveira220@gmail.com>
2026-02-18 22:26:52 -0300

    feat: update implementation of IdentityMapper
    add password and national ID complex mapping. Hashes password.

--- auth/src/main/java/dev/idinaldo/brabank/auth/infrastructure/mappers/IdentityMapper.java
+++ auth/src/main/java/dev/idinaldo/brabank/auth/infrastructure/mappers/IdentityMapper.java
@@ -1,4 +1,27 @@
 package dev.idinaldo.brabank.auth.infrastructure.mappers;
 
+import dev.idinaldo.brabank.auth.adapters.in.IdentityRegisterDTO;
+import dev.idinaldo.brabank.auth.domain.models.Identity;
+import dev.idinaldo.brabank.auth.domain.valueObjects.NationalId;
+import org.mapstruct.Mapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Mapper
 public class IdentityMapper {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public IdentityMapper(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Identity registerDtoToDomain(IdentityRegisterDTO identityRegisterDTO) {
+        Identity identity = new Identity();
+
+        identity.setPassword(this.passwordEncoder.encode(identityRegisterDTO.rawPassword()));
+        NationalId nationalId = new NationalId(identityRegisterDTO.nationalId());
+        identity.setNationalId(nationalId);
+
+        return identity;
+    }
 }
