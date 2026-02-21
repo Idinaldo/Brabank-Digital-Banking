f7cbbb72b36c805d5e93e993af7e7d7c1e8b5c45
Idinaldo <idinaldooliveira220@gmail.com>
2026-02-21 14:23:39 -0300

    feat: add initial implementation of Encryptor with AWS

---
+++ auth/src/main/java/dev/idinaldo/brabank/auth/ports/AwsEncryptorImpl.java
@@ -0,0 +1,14 @@
+package dev.idinaldo.brabank.auth.ports;
+
+public class AwsEncryptorImpl implements Encryptor {
+
+    @Override
+    public String encrypt(String plaintext) {
+        return "";
+    }
+
+    @Override
+    public String decrypt(String ciphertext, String encryptionKey) {
+        return "";
+    }
+}
