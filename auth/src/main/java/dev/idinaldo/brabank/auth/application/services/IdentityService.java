b5c4a5c6997ed3baedbff6fd5cf9dfc813e70241
Idinaldo <idinaldooliveira220@gmail.com>
2026-02-21 15:36:14 -0300

    feat: add initial implementation of IdentityService and registerClient method

--- auth/src/main/java/dev/idinaldo/brabank/auth/application/services/IdentityService.java
+++ auth/src/main/java/dev/idinaldo/brabank/auth/application/services/IdentityService.java
@@ -1,4 +1,21 @@
 package dev.idinaldo.brabank.auth.application.services;
 
-public class IdentityService {
+import dev.idinaldo.brabank.auth.application.usecases.ClientRegisterUseCase;
+import dev.idinaldo.brabank.auth.domain.models.Identity;
+import dev.idinaldo.brabank.auth.ports.IdentityRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IdentityService implements ClientRegisterUseCase {
+
+    private final IdentityRepository identityRepository;
+
+    public IdentityService(IdentityRepository identityRepository) {
+        this.identityRepository = identityRepository;
+    }
+
+    @Override
+    public void registerClient(Identity identity) {
+        this.identityRepository.save(identity);
+    }
 }
