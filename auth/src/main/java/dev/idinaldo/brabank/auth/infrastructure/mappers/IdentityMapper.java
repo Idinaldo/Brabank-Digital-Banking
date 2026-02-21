1eccc179c7f132fcf3a825753e56b3920e83b1b2
Idinaldo <idinaldooliveira220@gmail.com>
2026-02-21 14:33:49 -0300

    feat: add initial implementation of domainToEntity mapping

--- auth/src/main/java/dev/idinaldo/brabank/auth/infrastructure/mappers/IdentityMapper.java
+++ auth/src/main/java/dev/idinaldo/brabank/auth/infrastructure/mappers/IdentityMapper.java
@@ -1,27 +1,45 @@
 package dev.idinaldo.brabank.auth.infrastructure.mappers;
 
 import dev.idinaldo.brabank.auth.adapters.in.IdentityRegisterDTO;
+import dev.idinaldo.brabank.auth.adapters.out.entities.JpaIdentity;
 import dev.idinaldo.brabank.auth.domain.models.Identity;
 import dev.idinaldo.brabank.auth.domain.valueObjects.NationalId;
+import dev.idinaldo.brabank.auth.ports.Encryptor;
+import dev.idinaldo.brabank.auth.ports.MacGenerator;
 import org.mapstruct.Mapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Mapper
 public class IdentityMapper {
 
+    private final Encryptor encryptor;
+    private final MacGenerator macGenerator;
     private final PasswordEncoder passwordEncoder;
 
-    public IdentityMapper(PasswordEncoder passwordEncoder) {
+    public IdentityMapper(Encryptor encryptor, PasswordEncoder passwordEncoder, MacGenerator macGenerator) {
+        this.encryptor = encryptor;
         this.passwordEncoder = passwordEncoder;
+        this.macGenerator = macGenerator;
     }
 
     public Identity registerDtoToDomain(IdentityRegisterDTO identityRegisterDTO) {
         Identity identity = new Identity();
 
-        identity.setPassword(this.passwordEncoder.encode(identityRegisterDTO.rawPassword()));
+        identity.setPasswordHash(this.passwordEncoder.encode(identityRegisterDTO.rawPassword()));
+
         NationalId nationalId = new NationalId(identityRegisterDTO.nationalId());
         identity.setNationalId(nationalId);
 
         return identity;
+    }
+
+    public JpaIdentity domainToEntity(Identity identity) {
+        JpaIdentity jpaIdentity = new JpaIdentity();
+
+        jpaIdentity.setPasswordHash(identity.getPasswordHash());
+        jpaIdentity.setEncryptedNationalId(this.encryptor.encrypt(identity.getNationalId().value()));
+        jpaIdentity.setNationalIdMac(this.macGenerator.generateMac(identity.getNationalId().value()));
+
+        return jpaIdentity;
     }
 }
