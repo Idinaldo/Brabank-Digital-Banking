ece5b2e5552a55b6dd9019795420699ef6fbb257
Idinaldo <idinaldooliveira220@gmail.com>
2026-02-21 14:12:23 -0300

    feat: add implementation of IdentityRepository to handle persistence operations

---
+++ auth/src/main/java/dev/idinaldo/brabank/auth/adapters/out/repositories/IdentityRepositoryImpl.java
@@ -0,0 +1,6 @@
+package dev.idinaldo.brabank.auth.adapters.out.repositories;
+
+import dev.idinaldo.brabank.auth.ports.IdentityRepository;
+
+public class IdentityRepositoryImpl implements IdentityRepository {
+}
